aa96072a1b8e22f671faa11903613d6bd2657239
Zenith <micklepickle360@gmail.com>
2024-09-09 14:24:53 -0400

    update uwu

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ var r_text = [
 	"Alpha was here",
 	"Pizza with barbecue sauce, chicken, and bacon is just so good",
 	"Pineapple on pizza is good only if paired with meat",
-	"Realistically speaking, life is temporary. So give fuckall about the specifics and have as much fun as you can",
+	"Realistically speaking, life is temporary. So go and as much fun as you can",
 	"You're never alone in the microsoft maze",
 	"why are girls so pretty",
 	"This is not now in the hardware. It may never get into the hardware. After all, I hate music.",
